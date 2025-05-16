9fbb4b00bc2bbb1a3367376c6e7e21d45924ad28
ThomasMD <contact@thomas-marie-duval.fr>
2025-05-16 10:57:49 +0200

    patch couleurs redirection

--- src/pages/CommingSoonPage.tsx
+++ src/pages/CommingSoonPage.tsx
@@ -49,22 +49,22 @@ export default function CommingSoonPage() {
             {/* Réseaux sociaux*/}
             <div className="flex justify-center gap-5 my-3">
                 <a href="https://www.linkedin.com/in/thomas-marie-duval/" target="_blank" rel="noopener noreferrer"
-                   className="flex flex-col justify-center items-center w-12 hover:text-indigo-400">
-                    <img src="/icons/linkedin.svg" alt="Icone" className="size-6" />
-                    <p>LinkedIn</p>
+                   className="group flex flex-col justify-center items-center w-12">
+                    <img src="/icons/linkedin.svg" alt="Icone" className="size-6 group-hover:brightness-75 transition" />
+                    <p className={"group-hover:text-indigo-400"}>LinkedIn</p>
                 </a>
                 <a href="https://github.com/Vysty" target="_blank" rel="noopener noreferrer"
-                   className="flex flex-col justify-center items-center w-12 hover:text-indigo-400">
-                    <img src="/icons/github.svg" alt="Icone" className="size-6" />
-                    <p>GitHub</p>
+                   className="group flex flex-col justify-center items-center w-12">
+                    <img src="/icons/github.svg" alt="Icone" className="size-6 group-hover:brightness-75 transition" />
+                    <p className={"group-hover:text-indigo-400"}>GitHub</p>
                 </a>
-                <a href="mailto:contact@thomas-marie-duval.fr" className="flex flex-col justify-center items-center w-12 hover:text-indigo-400">
+                <a href="mailto:contact@thomas-marie-duval.fr" className="group flex flex-col justify-center items-center w-12">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" className="size-6">
+                         stroke="currentColor" className="size-6 group-hover:brightness-75 transition">
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"/>
                     </svg>
-                    <p>Email</p>
+                    <p className={"group-hover:text-indigo-400"}>Email</p>
                 </a>
             </div>
 

7e95434b18b70939780bf61b81af31da6e6fb9f7
ThomasMD <contact@thomas-marie-duval.fr>
2025-05-16 20:23:25 +0200

    Ajout des composants AppTile.tsx et EmptyTile.tsx

---
+++ src/assets/EmptyTile.tsx
@@ -0,0 +1,5 @@
+export default function EmptyTile() {
+    return (
+        <div className="w-32 h-32 rounded-lg flex flex-col items-center justify-center text-center shadow-md bg-gray-700"></div>
+    )
+}

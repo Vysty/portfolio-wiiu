7df896ee88a30be1e9ab36eeed2e0aca452d0327
ThomasMD <contact@thomas-marie-duval.fr>
2025-05-16 20:28:01 +0200

    Ajout d'un import

--- src/pages/WiiUPortfolio.tsx
+++ src/pages/WiiUPortfolio.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import AppTile from '../assets/AppTile.tsx'
 import EmptyTile from '../assets/EmptyTile.tsx'

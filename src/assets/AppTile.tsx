5b2a572ef4fde05b9ddcd308b63dd4b869513a8b
ThomasMD <contact@thomas-marie-duval.fr>
2026-05-03 22:08:41 +0200

    Add Bubble component for tooltip display in AppTile

--- src/assets/AppTile.tsx
+++ src/assets/AppTile.tsx
@@ -1,24 +1,13 @@
 import { motion, type Variants } from 'framer-motion'
 import React from 'react'
 import { tileVariants } from '../styles/FramerConsts.ts'
+import Bubble from './Bubble.tsx'
 
 export type AppTileProps = {
     label: string
     icon: string
     content: React.ReactNode
     onOpen: (content: React.ReactNode) => void
-}
-
-// Définition des Variants pour la bulle de texte si on l'affiche ou pas
-const bubbleVariants: Variants = {
-    rest: { opacity: 0, y: 10, scale: 0.8, display: 'none' },
-    hover: {
-        opacity: 1,
-        y: -10, // La bulle monte légèrement
-        scale: 1,
-        display: 'block',
-        transition: { type: 'spring', stiffness: 300, damping: 20 },
-    },
 }
 
 export default function AppTile({
@@ -36,21 +25,7 @@ export default function AppTile({
             // relative pour ne pas couper la bulle de texte
             className={`relative w-44 h-44 rounded-lg flex flex-col items-center justify-center cursor-pointer shadow-md border-12 border-tilescolor/80 bg-tilescolor/80 hover:border-tileselected hover:bg-tileselected`}
         >
-            {/* --- Bulle de texte variants hover --- */}
-            {/*Possiblement remettre sur la bulle : "left-1/2 -translate-x-1/2" ???*/}
-            <motion.div
-                variants={bubbleVariants}
-                className="absolute -top-16 w-max max-w-[180px] pointer-events-none"
-            >
-                <div className="bg-white text-gray-800 px-4 py-2 rounded-2xl shadow-xl border-2 border-gray-100 flex flex-col items-center">
-                    <span className="text-lg font-sans font-semibold whitespace-nowrap">
-                        {label}
-                    </span>
-                    {/* Le petit triangle (flèche) en bas de la bulle */}
-                    <div className="absolute -bottom-2 w-4 h-4 bg-white rotate-45 border-r-2 border-b-2 border-gray-100 translate-y-[-2px]"></div>
-                </div>
-            </motion.div>
-            {/* --- FIN DE LA BULLE--- */}
+            <Bubble pos={'top'} label={label} />
 
             <img
                 src={icon}
---
+++ src/assets/Bubble.tsx
@@ -0,0 +1,45 @@
+import { motion, Variants } from 'framer-motion'
+
+// Définition des Variants pour la bulle de texte si on l'affiche ou pas
+const bubbleVariants: Variants = {
+    rest: { opacity: 0, y: 10, scale: 0.8, display: 'none' },
+    hover: {
+        opacity: 1,
+        y: -10, // La bulle monte légèrement
+        scale: 1,
+        display: 'block',
+        transition: { type: 'spring', stiffness: 300, damping: 20 },
+    },
+}
+
+export default function Bubble({
+    pos,
+    label,
+}: {
+    pos: 'top' | 'bottom'
+    label: string
+}) {
+    const bubblePositionClass = pos === 'top' ? '-top-16' : '-bottom-16'
+
+    const arrowPositionClass =
+        pos === 'top'
+            ? '-bottom-2 border-r-2 border-b-2 translate-y-[-2px]'
+            : '-top-2 border-t-2 border-l-2 translate-y-[2px]'
+
+    return (
+        <motion.div
+            variants={bubbleVariants}
+            className={`absolute ${bubblePositionClass} w-max max-w-[180px] pointer-events-none`}
+        >
+            <div className="bg-white text-gray-800 px-4 py-2 rounded-2xl shadow-xl border-2 border-gray-100 flex flex-col items-center">
+                <span className="text-lg font-sans font-semibold whitespace-nowrap">
+                    {label}
+                </span>
+                {/* Le petit triangle (flèche) dynamique */}
+                <div
+                    className={`absolute w-4 h-4 bg-white rotate-45 border-gray-100 transform ${arrowPositionClass}`}
+                ></div>
+            </div>
+        </motion.div>
+    )
+}

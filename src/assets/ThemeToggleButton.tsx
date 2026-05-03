bd079e37a4dae2af575a58b01472bb2304169b28
ThomasMD <contact@thomas-marie-duval.fr>
2026-05-03 22:03:41 +0200

    Modification du bouton de thème

--- src/assets/ThemeToggleButton.tsx
+++ src/assets/ThemeToggleButton.tsx
@@ -2,6 +2,47 @@ import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { tileVariants } from '../styles/FramerConsts.ts'
 
+const SunIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <circle cx="12" cy="12" r="4" />
+        <path d="M12 2v2" />
+        <path d="M12 20v2" />
+        <path d="m4.93 4.93 1.41 1.41" />
+        <path d="m17.66 17.66 1.41 1.41" />
+        <path d="M2 12h2" />
+        <path d="M20 12h2" />
+        <path d="m6.34 17.66-1.41 1.41" />
+        <path d="m19.07 4.93-1.41 1.41" />
+    </svg>
+)
+
+const MoonIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+    </svg>
+)
+// ----------------------------------------
+
 export default function ThemeToggleButton() {
     const [theme, setTheme] = useState<'light' | 'dark' | null>(null)
 
@@ -41,9 +82,15 @@ export default function ThemeToggleButton() {
             whileHover="hover"
             variants={tileVariants}
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            className="px-6 py-4 rounded-lg bg-tilescolor hover:bg-foreground hover:text-tilescolor text-backforeground transition duration-300 hover:cursor-pointer"
+            aria-label={
+                theme === 'dark'
+                    ? 'Passer au thème clair'
+                    : 'Passer au thème sombre'
+            }
+            className="p-4 rounded-2xl flex items-center justify-center cursor-pointer shadow-md transition-colors duration-300 text-foreground bg-background hover:bg-foreground hover:text-background  border-12 border-tilescolor/80 hover:border-tileselected"
         >
-            {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
+            {/* Si on est en Dark, on montre le soleil pour passer en Light. Sinon, la lune. */}
+            {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
         </motion.button>
     )
 }

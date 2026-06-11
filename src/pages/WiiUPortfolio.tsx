0051d39e7f821a172947625b0b871b6089a31b5b
ThomasMD <contact@thomas-marie-duval.fr>
2026-06-11 19:37:59 +0200

    augmentation du temps de chargement

--- src/pages/WiiUPortfolio.tsx
+++ src/pages/WiiUPortfolio.tsx
@@ -124,7 +124,7 @@ export default function WiiUPortfolio() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false)
-    }, 1500) // 2000 millisecondes = 2 secondes
+    }, 2500) // 2000 millisecondes = 2 secondes
 
     return () => clearTimeout(timer)
   }, [])
